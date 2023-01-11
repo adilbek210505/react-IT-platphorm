04b5ebfb92b12de7fd221e0ad0f174c438073f6b
adilbek210505 <116545639+adilbek210505@users.noreply.github.com>
2023-01-11 13:58:55 +0600

    accordeon change

--- src/Companents/home/AboutSchool.jsx
+++ src/Companents/home/AboutSchool.jsx
@@ -9,7 +9,12 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 const AboutSchool = () => {
 
-
+    const settings = {
+        dots: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 1
+    };
 
     return (
         <section id="school">
