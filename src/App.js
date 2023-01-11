1a1c2689aa89497d46a97bf710505437af39633d
adil <116545639+adilbek210505@users.noreply.github.com>
2023-01-11 18:58:27 +0600

    change (#7)

--- src/App.js
+++ src/App.js
@@ -5,18 +5,22 @@ import Main from "./Companents/home/Main";
 import AboutSchool from "./Companents/home/AboutSchool";
 import AboutUs from "./Companents/home/AboutUs";
 import OurCourses from "./Companents/home/OurCourses";
+import Layout from "./Companents/Layout/Layout";
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-        <Routes>
-            <Route path={"/"} element={<Main/>}/>
-            <Route path={'/aboutSchool'} element={<AboutSchool/>}/>
-            <Route path={'/ourCourses'} element={<OurCourses/>}/>
-            <Route path={'/AboutUs'} element={<AboutUs/>}/>
-        </Routes>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Layout>
+                <Routes>
+                    <Route path={"/"} element={<Main/>}/>
+                    <Route path={'/aboutSchool'} element={<AboutSchool/>}/>
+                    <Route path={'/ourCourses'} element={<OurCourses/>}/>
+                    <Route path={'/AboutUs'} element={<AboutUs/>}/>
+                </Routes>
+            </Layout>
+        </div>
+    );
 }
 
 export default App;
