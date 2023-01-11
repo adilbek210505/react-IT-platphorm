e43ca9c91b573658b1f5067e2d3c4e4848e6cd57
adilbek210505 <116545639+adilbek210505@users.noreply.github.com>
2023-01-11 18:57:19 +0600

    change

---
+++ src/Companents/follow/Follow.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Follow = () => {
+
+    return (
+        <section id="Follow">
+            <h1>Главная / Оформить подписку</h1>
+            <div className="container">
+                <div className="follow">
+                    <h1>Год</h1>
+                    <input type="text"/>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Follow;
