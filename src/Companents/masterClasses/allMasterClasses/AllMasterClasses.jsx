e90112fa7ba3809d7f0479b494a32c65681b72c6
aayemm98 <begaitj8@gmail.com>
2023-01-19 14:17:03 +0600

    commit pull

--- src/Companents/masterClasses/allMasterClasses/AllMasterClasses.jsx
+++ src/Companents/masterClasses/allMasterClasses/AllMasterClasses.jsx
@@ -3,7 +3,45 @@ import React from 'react';
 const AllMasterClasses = () => {
     return (
         <div className={'allMasterClasses'}>
-             все мастер классы
+       <section id='all'>
+           <div className='all'>
+               <div className='all--home'>
+                   <p>Главная / <span>Мастер классы</span></p>
+               </div>
+               <div className='master'>
+                   <div className='classes' >
+                       <h2 className='classes--title'>Мастер классы</h2>
+                   </div>
+               <div className="master--global">
+                   <div className="master--global__block">
+                       <p className='master--global__block--text'>Реактивное программирование на Java: как, зачем и стоит ли? </p>
+                       <span className='master--global__block--p'>Программирования появилась сравнительно недавно, лет 10 назад. Что вызвало популярность этого относительно нового подхода и почему сейчас он в тренде, рассказал на конференции РИТ++ ...</span>
+                   </div>
+                   <div className="master--global__block">
+                       <p className='master--global__block--text'>Олимпиада для айтишников: как мотивировать молодых разработчиков
+                       </p>
+                       <span className='master--global__block--p'> В современном HR широко распространены событийные мотивационные мероприятия, как внешние, так и внутренние – различные социальные проекты, выставки, праздники, мастер-классы и тренинги, спортивные и ЗОЖ.</span>
+                   </div>
+                   <div className="master--global__block">
+                       <p className='master--global__block--text'>Семинар Дино Эспозито “Real-time Data Coming at Last in ASP.NET Core”</p>
+                       <span className='master--global__block--p'>Евангелистом разработки под Android и на Kotlin в JetBrains, а также членом команды, которая ведет WURFL, базу данных с информацией о мобильных устройствах, которая используется такими компаниями....</span>
+                   </div>
+                   <div className="master--global__block">
+                       <p className='master--global__block--text'>DevOps конференция в Москве </p>
+                       <span className='master--global__block--p'> Сборки на Jenkins с закрытыми глазами? А настроить Docker без смс, переплат и регистрации? Что будет, если билд соберется в течение одной миллисекунды? Эти и другие паранормальные явления слетятся обсудить более 400 человек...</span>
+                   </div>
+                   <div className="master--global__block">
+                       <p className='master--global__block--text'>Олимпиада для айтишников: как мотивировать молодых разработчиков</p>
+                       <span className='master--global__block--p'>В современном HR широко распространены событийные мотивационные мероприятия, как внешние, так и внутренние – различные социальные проекты, выставки, праздники, мастер-классы и тренинги, спортивные и ЗОЖ.</span>
+                   </div>
+                   <div className="master--global__block">
+                       <p className='master--global__block--text'>Как научиться учиться.</p>
+                       <span className='master--global__block--p'>Только за своим столом, но и в других местах - чтобы в непривычной ситуации нужная информация не вылетела из головы. Перед экзаменом, тестом, сертификацией можно повторять “по нарастающей” - каждый день чуть больше, чем в предыдущий...</span>
+                   </div>
+               </div>
+               </div>
+           </div>
+       </section>
         </div>
     );
 };
--- src/Companents/masterClasses/detailMasterClasses/DetailMasterClasses.jsx
+++ src/Companents/masterClasses/detailMasterClasses/DetailMasterClasses.jsx
@@ -1,9 +1,162 @@
 import React from 'react';
+import programm from '../../image/programm.detail.png'
+import person from '../../image/person.detail.png'
 
 const DetailMasterClasses = () => {
     return (
         <div className='detailMasterClasses'>
-            мастер классы до подписки
+            <div className='container'>
+            {/*мастер классы до подписки*/}
+            <section id='detail'>
+                <div className='detail'>
+                    <div className='class'>
+                    <p>Главная / Мастер классы  <span>/ Реактивное программирование на Java</span></p>
+                    </div>
+                    <div className='java'>
+                        <div className='program'>
+                            <h2>Реактивное программирование на Java : как, зачем и стоит ли?  Часть 1 </h2>
+                            <p>Идея реактивного программирования появилась сравнительно недавно, лет 10 назад. Что вызвало популярность этого относительно нового подхода и почему сейчас он в тренде, рассказал на конференции </p>
+                            <button>Купить мастер-класс за 46$</button>
+                        </div>
+                        <div className='access'>
+                           <div className='access--6'>
+                               <p>Доступ: </p>
+                               <span>6 недель</span>
+                           </div>
+                            <div className='access--5'>
+                                <p>В мастер-класс входит: </p>
+                                <span>5 уроков</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section id='global'>
+                <div className='dd'>
+                <div className="dd--which">
+                 <p>Что, как и почему</p>
+                 </div>
+                <div className="dd--mode">
+                    <div className='week'>
+                        <div className="week--titles">
+                            <h1>О МАСТЕР-КЛАССЕ</h1>
+                            <p>В режиме мастер-класса он продемонстрировал, почему так важен неблокирующий ввод-вывод, в чем минусы классической многопоточности, в каких ситуациях нужна реактивность, и что она может дать. А еще описал недостатки реактивного подхода.</p>
+                            <button>Купить мастер-класс </button>
+                        </div>
+                      <div className='repeat'>
+                    <div className='repeat--all'>
+                        <p>Доступ: </p>
+                        <span>6 недель</span>
+                    </div>
+                    <div className='repeat--all'>
+                        <p>Доступ: </p>
+                        <span>6 недель</span>
+                    </div>
+                      </div>
+                    </div>
+
+                    <div className="texts">
+                        <h5>Переходите к видеоурокам, что бы научится: </h5>
+                        <div className="texts--p">
+                            <div className="dote"></div>
+                            <p>Чтению  файла (blocked on reading file);
+                            </p>
+                        </div>
+                    </div>
+                    <div className="btn1">
+                        <button>Купить мастер-класс </button>
+                        <button>Смотреть программу</button>
+                    </div>
+                </div>
+                </div>
+            </section>
+
+            <section id="program">
+                <div className='reactivity'>
+                        <div className='example'>
+                            <h1>ПРОГРАММА МАСТЕР-КЛАССА</h1>
+                            <button className='blocks'><p>1</p> <span>Reactivity</span></button>
+                            <button className='blocks'><p>2</p> <span>Reactive approach</span></button>
+                            <button className='blocks'><p>3</p> <span>Observable example</span></button>
+                            <button className='blocks'><p>4</p> <span>Implementing and subscribing to an observer</span></button>
+                            <button className='blocks'><p>5</p> <span>Reactive Streams spec</span></button>
+                            <div className='buy'>
+                                <button>Купить мастер-класс </button>
+                            </div>
+                        </div>
+                        <img src={programm} alt="" className='programmer'/>
+                    </div>
+
+            </section>
+
+            <section id='front'>
+                <div className='front'>
+                    <img src="{person}" alt="" className='human'/>
+                    <div className='title'>
+                        <p>Мастер класс ведет</p>
+                        <h3>Евгений Александрович</h3>
+                        <span> Frontend developer / Аратор </span>
+                    </div>
+                </div>
+            </section>
+
+                <section id='process'>
+                    <div className="process">
+                   <div className="education">
+                       <h2>Процесс обучения</h2>
+                       <p>Каждый из наших мастер-классов состоит из 2-8 уроков. Рекомендуем проходить мастер-класс последовательно, урок за уроком</p>
+                   </div>
+                       <div className='main'>
+                           <div className='react'>
+                               <div className='reactive'>
+                                   <div className='timetable'>
+                                       <div className='block'>
+                                           <h3>1</h3>
+                                           <h6>Мастер-класс</h6>
+                                           <p>Вы получите доступ ко всем урокам мастер-класса и другим дополнительным материалам</p>
+                                       </div>
+                                   </div>
+                               </div>
+                               <div className='reactive'>
+                                   <div className='timetable'>
+                                       <div className='block'>
+                                           <h3>1</h3>
+                                           <h6>Мастер-класс</h6>
+                                           <p>Вы получите доступ ко всем урокам мастер-класса и другим дополнительным материалам</p>
+                                       </div>
+                                   </div>
+                               </div>
+                               <div className='reactive'>
+                                   <div className='timetable'>
+                                       <div className='block'>
+                                           <h3>1</h3>
+                                           <h6>Мастер-класс</h6>
+                                           <p>Вы получите доступ ко всем урокам мастер-класса и другим дополнительным материалам</p>
+                                       </div>
+                                   </div>
+                               </div>
+                               <div className='reactive'>
+                                   <div className='timetable'>
+                                       <div className='block'>
+                                           <h3>1</h3>
+                                           <h6>Мастер-класс</h6>
+                                           <p>Вы получите доступ ко всем урокам мастер-класса и другим дополнительным материалам</p>
+                                       </div>
+                                   </div>
+                               </div>
+                           </div>
+                           <div className='blocks'>
+                               <div className='part'>
+                                   <h5>Мастер-класс “Реактивное программирование на Java : как, зачем и стоит ли? Часть 1”</h5>
+                                   <h2>46,00 $</h2>
+                               </div>
+                           </div>
+                       </div>
+                    </div>
+
+                </section>
+            </div>
         </div>
     );
 };
